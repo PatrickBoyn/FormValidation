7ea1180d54ffc6c0a51d1c1e9ed8b7c70a805f1b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-08 22:44:15 -0400

    Added a class depending on the regex test.

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ document.getElementById('phone').addEventListener('blur', validatePhoneNumber);
 function validateName() {
   const name = document.getElementById('name');
   const re = /^[a-zA-Z]{2, 10}$/;
+
   if (!re.test(name.value)) {
+    name.classList.add('is-invalid');
   } else {
+    name.classList.remove('is-invalid');
   }
 }
 
