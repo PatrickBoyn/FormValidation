017f5c53197adb11eb97f69291b16115919ced2c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-09 01:52:16 -0400

    Fixed the regex. Also logged the results.

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ invalidFeedback.forEach(element => {
 
 function validateName() {
   const name = document.getElementById('name');
-  const re = /^[a-zA-Z]{2, 10}$/;
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  console.log(name.value);
+  console.log(re.test(name.value));
 
   if (!re.test(name.value)) {
     name.classList.add('is-invalid');
     invalidAdd.style.display = 'block';
   } else {
     name.classList.remove('is-invalid');
+    invalidAdd.style.display = 'none';
   }
 }
 
