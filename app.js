7467f7e69aad533f3090906696857f5ef45ab3ef
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-08 22:40:54 -0400

    Added  a check for the name.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ document.getElementById('phone').addEventListener('blur', validatePhoneNumber);
 function validateName() {
   const name = document.getElementById('name');
   const re = /^[a-zA-Z]{2, 10}$/;
+  if (!re.test(name.value)) {
+  } else {
+  }
 }
 
 function validateZipCode() {}
