fe3c285dfc76ce664b0a90733584cf5d18bc4e50
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-09 02:28:07 -0400

    Added the test for the regex.

--- app.js
+++ app.js
@@ -59,4 +59,12 @@ function validateEmail() {
 function validatePhoneNumber() {
   const phone = document.getElementById('phone');
   const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+    invalidFeedback[3].style.display = 'block';
+  } else {
+    phone.classList.remove('is-invalid');
+    invalidFeedback[3].style.display = 'none';
+  }
 }
