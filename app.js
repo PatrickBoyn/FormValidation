4d9df3bf833a500ad06a452501075d04e95c8b8e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-08 22:36:38 -0400

    Added the four functions.

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ document.getElementById('name').addEventListener('blur', validateName);
 document.getElementById('zip-code').addEventListener('blur', validateZipCode);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhoneNumber);
+
+function validateName() {}
+
+function validateZipCode() {}
+
+function validateEmail() {}
+
+function validatePhoneNumber() {}
